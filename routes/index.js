b44221a4954bcc249dca24509449f7701fe9c7a3
Mark Ni <xixian.ni@gmail.com>
2013-12-04 22:35:06 -0800

    bump version number

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@
  */
 
 exports.index = function(req, res){
-  var v = {version:'0.1.3-5'};
+  var v = {version:'0.1.4-5'};
   //grunt-version fails to work with grunt-bumpx
   res.render('index', { title: 'inTouch 2 Alpha v'+ v.version.split('-')[0]+' build '+(parseInt(v.version.split('-')[1])+1) });
 };
