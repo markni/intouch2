ae821c7ffedf7d568f10978d2f8ef5ed036943da
Mark Ni <xixian.ni@gmail.com>
2013-12-08 09:37:07 -0800

    basic function of subject page (rendering title and ep titles)

--- public/js/controllers/subject.js
+++ public/js/controllers/subject.js
@@ -1,5 +1,21 @@
-app.controller('subjectCtrl', function ($translate, $scope, Auth, $http, $location, $cookieStore, Helpers, $timeout) {
+app.controller('subjectCtrl', function ($translate, $scope, Auth, $http, $location, $cookieStore, Helpers, $routeParams) {
     $scope.loading = 0;
 
+    $scope.getRating = function(){
+        return parseInt($scope.subject)
 
-})
+    }
+
+    $http({method: 'POST', url: '/api/subject/' + $routeParams.id}).
+        success(function (data, status) {
+            $scope.subject = data;
+
+            $scope.loading--;
+        }).
+        error(function (data, status) {
+            $scope.loading--;
+
+        });
+
+
+});
