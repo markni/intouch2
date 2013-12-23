312e0590ed07675bbfb0dcdc031664565a9c4cd4
Mark Ni <xixian.ni@gmail.com>
2013-12-23 10:14:36 -0800

    get some social share button

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,5 @@
 exports.index = function(req, res){
   var v = {version:'0.1.4-7'};
   //grunt-version fails to work with grunt-bumpx
-  res.render('index', { title: 'inTouch 2 Alpha v'+ v.version.split('-')[0]+' build '+(parseInt(v.version.split('-')[1])+1) });
+  res.render('index', { title: 'inTouch'});
 };
